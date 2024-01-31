1869e6f6c5e975ad3700394928a4f7ede0c07ce3
Jules <ju.pabst@gmail.com>
2024-01-31 11:34:32 +0100

    added diamond images

--- I-am-rich/I-am-rich/ContentView.swift
+++ I-am-rich/I-am-rich/ContentView.swift
@@ -9,10 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("I am rich!")
+        ZStack {
+            Color(.systemTeal).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                Text("I am rich!")
+                    .fontWeight(.bold).font(.system(size: 50)).foregroundColor(Color(hue: 1.0, saturation: 0.0, brightness: 1.0))
+                Image("Icon-1024")
+                
+            }
         }
-        .padding().font(.system(size: 50))
     }
 }
 
