aad37e69b2d27eb1c64582e3254ffba2aba3de28
Jules <ju.pabst@gmail.com>
2024-01-31 10:55:39 +0100

    deleted old project and added new project that is of type SwiftUI

---
+++ I-am-rich/I-am-rich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  I-am-rich
+//
+//  Created by Julia Pabst on 31.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ I-am-rich/I-am-rich/I_am_richApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_richApp.swift
+//  I-am-rich
+//
+//  Created by Julia Pabst on 31.01.24.
+//
+
+import SwiftUI
+
+@main
+struct I_am_richApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
