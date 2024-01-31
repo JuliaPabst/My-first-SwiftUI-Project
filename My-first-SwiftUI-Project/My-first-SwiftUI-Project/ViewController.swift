a20af42ce759ce06c2bf15f441a96f4168c74025
Jules <ju.pabst@gmail.com>
2024-01-31 10:52:43 +0100

    created xCode project

---
+++ My-first-SwiftUI-Project/My-first-SwiftUI-Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My-first-SwiftUI-Project
+//
+//  Created by Julia Pabst on 31.01.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
